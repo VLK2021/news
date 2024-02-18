57c96191ea3c370d3667752edea7252c14125c21
vlk2022 <volodymyrkostiuk2021@gmail.com>
2024-02-19 00:37:44 +0200

    change key

--- src/constants/baseURL.js
+++ src/constants/baseURL.js
@@ -1,3 +1,4 @@
 const baseURL = 'https://newsapi.org/v2';
+// const baseURL = 'https://cors-anywhere.herokuapp.com/https://newsapi.org/v2';
 
 export default baseURL;
--- src/services/news.service.js
+++ src/services/news.service.js
@@ -3,53 +3,53 @@ import {urls} from "../constants/urls";
 import {apiKey} from "../constants/apiKey";
 
 
-// const newsService = {
-//     getTopUA: () => axiosService
-//         .get(`${urls.topHeadlines}?country=ua&apiKey=${apiKey}`)
-//         .then(value => value.data),
-//
-//     getTopUS: () => axiosService
-//         .get(`${urls.topHeadlines}?country=us&apiKey=${apiKey}`)
-//         .then(value => value.data),
-//
-//     getTopPL: () => axiosService
-//         .get(`${urls.topHeadlines}?country=pl&apiKey=${apiKey}`)
-//         .then(value => value.data),
-//
-//     getEverything: (current, page) => axiosService
-//         .get(`${urls.everything}?q=${current}&apiKey=${apiKey}&pageSize=10&page=${page}`)
-//         .then(value => value.data),
-//
-//     getSearchEverything: (current, page) => axiosService
-//         .get(`${urls.everything}?q=${current}&apiKey=${apiKey}&pageSize=30&page=${page}`)
-//         .then(value => value.data),
-//
-// }
-
-
-
 const newsService = {
     getTopUA: () => axiosService
-        .get(`${urls.topHeadlines}?country=ua`)
+        .get(`${urls.topHeadlines}?country=ua&apiKey=${apiKey}`)
         .then(value => value.data),
 
     getTopUS: () => axiosService
-        .get(`${urls.topHeadlines}?country=us`)
+        .get(`${urls.topHeadlines}?country=us&apiKey=${apiKey}`)
         .then(value => value.data),
 
     getTopPL: () => axiosService
-        .get(`${urls.topHeadlines}?country=pl`)
+        .get(`${urls.topHeadlines}?country=pl&apiKey=${apiKey}`)
         .then(value => value.data),
 
     getEverything: (current, page) => axiosService
-        .get(`${urls.everything}?q=${current}&pageSize=10&page=${page}`)
+        .get(`${urls.everything}?q=${current}&apiKey=${apiKey}&pageSize=10&page=${page}`)
         .then(value => value.data),
 
     getSearchEverything: (current, page) => axiosService
-        .get(`${urls.everything}?q=${current}&pageSize=30&page=${page}`)
+        .get(`${urls.everything}?q=${current}&apiKey=${apiKey}&pageSize=30&page=${page}`)
         .then(value => value.data),
 
 }
+
+
+
+// const newsService = {
+//     getTopUA: () => axiosService
+//         .get(`${urls.topHeadlines}?country=ua`)
+//         .then(value => value.data),
+//
+//     getTopUS: () => axiosService
+//         .get(`${urls.topHeadlines}?country=us`)
+//         .then(value => value.data),
+//
+//     getTopPL: () => axiosService
+//         .get(`${urls.topHeadlines}?country=pl`)
+//         .then(value => value.data),
+//
+//     getEverything: (current, page) => axiosService
+//         .get(`${urls.everything}?q=${current}&pageSize=10&page=${page}`)
+//         .then(value => value.data),
+//
+//     getSearchEverything: (current, page) => axiosService
+//         .get(`${urls.everything}?q=${current}&pageSize=30&page=${page}`)
+//         .then(value => value.data),
+//
+// }
 
 export {
     newsService
