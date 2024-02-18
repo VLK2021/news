d10d3b8f5e45665611e9f05820befc9a46951b18
vlk2022 <volodymyrkostiuk2021@gmail.com>
2024-02-19 00:58:53 +0200

    change key

--- src/services/axios.service.js
+++ src/services/axios.service.js
@@ -12,7 +12,8 @@ const axiosService = axios.create({
     baseURL,
     headers: {
         Authorization: 'b4d0ac35ee9444868db03f8f369feec1'
-    }
+    },
+    withCredentials: true
 });
 
 export {
